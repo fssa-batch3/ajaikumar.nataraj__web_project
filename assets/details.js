18e389a6ec7443e459627c7e462bc22ecf197284
Ajaikumar-N <ajaikumar.nataraj@fssa.freshworks.com>
2023-04-10 14:30:15 +0530

    chore: add designs

--- assets/details.js
+++ assets/details.js
@@ -57,299 +57,278 @@
 //   document.querySelector("body").append(footer);
 // }
 
-//js tabs code start
-function openCity(evt, cityName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace("active", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
+// function to send comment and to set in the local storage
+function send() {
+  const msginput = document.getElementById("msg").value;
+  const coverpic = user_id["user_pic"];
+  const hours = new Date().getHours();
+  const minutes = new Date().getMinutes();
+  const currentTime = `${hours}:${minutes}`;
 
-  document.getElementById("default").style.display = "none";
-}
+  const Userid = user_id["userid"];
 
-//return to home tabs function
-function homeon() {
-  document.getElementById("default").style.display = "block";
-  document.getElementById("my-courses").style.display = "none";
-  document.getElementById("blogs").style.display = "none";
-  document.getElementById("latest-courses").style.display = "none";
-  document.getElementById("youtube-videos").style.display = "none";
-  document.getElementById("free-courses").style.display = "none";
-  document.getElementById("live-courses").style.display = "none";
-}
+  const username = user_id["user_name"];
 
-//blogs data JSON
-const blogs_data = [
-  {
-    img: "https://tradeciety.com/hubfs/Imported_Blog_Media/947cUtXZwI-HD.jpeg",
-    blog_title: "How Professional Traders use..",
-    blog_timing: "5 Min Read",
-    blog_language: "English",
-    blog_hashtag: " #finance #forex",
-    blog_cost: "Read for FREE",
-  },
-  {
-    img: "https://tradeciety.com/hubfs/Folie13.png",
-    blog_title: "6 Forex Price Action Trading..",
-    blog_timing: "5 Min Read",
-    blog_language: "English",
-    blog_hashtag: " #finance #forex",
-    blog_cost: "Read for FREE",
-  },
-  {
-    img: "https://tradeciety.com/hubfs/cuphandle%20%282%29.png",
-    blog_title: "Learn How To Use The Stocha..",
-    blog_timing: "5 Min Read",
-    blog_language: "English",
-    blog_hashtag: " #finance #money",
-    blog_cost: "Read for FREE",
-  },
-  {
-    img: "https://tradeciety.com/hubfs/Imported_Blog_Media/Stoch_MA-1.png",
-    blog_title: "3 Great Reversal Strategies..",
-    blog_timing: "5 Min Read",
-    blog_language: "English",
-    blog_hashtag: " #money #Indicator",
-    blog_cost: "Read for FREE",
-  },
-  {
-    img: "https://tradeciety.com/hubfs/XAUUSD_2023-01-11_11-36-40.png",
-    blog_title: "8 must know PRICE ACTION..",
-    blog_timing: "5 Min Read",
-    blog_language: "English",
-    blog_hashtag: " #finance #money",
-    blog_cost: "Read for FREE",
-  },
-  {
-    img: "https://tradeciety.com/hubfs/Imported_Blog_Media/OvBP_gJnU9E-HD-1.jpeg",
-    blog_title: "Start Trading like a Pro with..",
-    blog_timing: "5 Min Read",
-    blog_language: "English",
-    blog_hashtag: "#PriceAction",
-    blog_cost: "Read for FREE",
-  },
-  {
-    img: "https://tradeciety.com/hubfs/Imported_Blog_Media/aDih3RZkt9I-HD-1.jpeg",
-    blog_title: "How to Trade STRONG Trends..",
-    blog_timing: "5 Min Read",
-    blog_language: "English",
-    blog_hashtag: " #finance #money",
-    blog_cost: "Read for FREE",
-  },
-  {
-    img: "https://tradeciety.com/hubfs/Imported_Blog_Media/OY-RwsKwm2o-HD.jpeg",
-    blog_title: "Ultimate free Price Action..",
-    blog_timing: "5 Min Read",
-    blog_language: "English",
-    blog_hashtag: " #finance #money",
-    blog_cost: "Read for FREE",
-  },
-  {
-    img: "https://tradeciety.com/hubfs/Imported_Blog_Media/JDpYMDGawMI-HD.jpeg",
-    blog_title: "Chart Pattern Trading – Full..",
-    blog_timing: "5 Min Read",
-    blog_language: "English",
-    blog_hashtag: " #finance #money",
-    blog_cost: "Read for FREE",
-  },
-  {
-    img: "https://tradeciety.com/hubfs/Imported_Blog_Media/Ethcv9JS4Zs-HD.jpeg",
-    blog_title: "9 best Day Trading tips to use..",
-    blog_timing: "5 Min Read",
-    blog_language: "English",
-    blog_hashtag: " #finance #money",
-    blog_cost: "Read for FREE",
-  },
-];
+  const fundraiseid = fundraiseDetails["product_id"];
 
-//blogs data card create
-let course_div4;
-for (i = 0; i < blogs_data.length; i++) {
-  course_div4 = document.createElement("div");
-  course_div4.setAttribute("class", "first-course3");
-  course_div4.setAttribute(
-    "href",
-    "details.html?name=" + blogs_data[i]["blog_title"]
-  );
-  course_div4.innerHTML = `<div>
-                       <img class="course-img" src=${blogs_data[i]["img"]} alt="">
-                   </div>
-                   <div>
-                       <h3 class="course-title">${blogs_data[i]["blog_title"]}</h3>
-                       <div class="course-details">
-                           <p class="course-timing">${blogs_data[i]["blog_timing"]}</p>
-                           <p class="course-ln">${blogs_data[i]["blog_language"]}</p>
-                           <p class="course-enrolled">${blogs_data[i]["blog_hashtag"]}</p>
-                       </div>
-                    <h3 class="course-cost">${blogs_data[i]["blog_cost"]}</h3>
-                   </div> `;
-  //  console.log(course_div4);
+  const commentid = Date.now();
 
-  document.querySelector(".scroll-courses-div4").append(course_div4);
-
-  //search query for search bar code start
-
-  let searchbar = document.getElementById("searchbar");
-  const cards = document.getElementsByClassName("first-course3");
-
-  searchbar.addEventListener("input", () => {
-    for (let i = 0; i < cards.length; i++) {
-      const element = cards[i];
-      if (
-        element.innerHTML.toLowerCase().includes(searchbar.value.toLowerCase())
-      ) {
-        element.style.display = "block";
-      } else {
-        element.style.display = "none";
-      }
-    }
-  });
-}
-
-//search query for search bar code end
-
-// All courses card create
-
-const course_data = JSON.parse(localStorage.getItem("course_data"));
-
-let course_div_stocks;
-
-for (i = 0; i < course_data.length; i++) {
-  course_div_stocks = document.createElement("div");
-  course_div_stocks.setAttribute("class", "first-course2-stocks");
-  // course_div_stocks.setAttribute("href","details.html?name=" + course_data[i]["title"]);
-  let cost = "";
-  let discount = "";
-
-  if (course_data[i]["cost"] === "learn for free") {
-    cost += " ";
-    discount += 100;
+  if (msginput == "" || msginput == null) {
+    alert("You can't send empty comment");
+    return;
   } else {
-    cost += "₹" + " ";
-    discount +=
-      100 - (course_data[i]["cost"] / course_data[i]["old_cost"]) * 100;
+    let commentObj = {
+      userpic: coverpic,
+      userName: username,
+      user_ID: Userid,
+      fundraiseId: fundraiseid,
+      commentId: commentid,
+      time: currentTime,
+      msg: msginput,
+    };
+    console.log(commentObj);
+    commentArr.push(commentObj);
+    window.localStorage.setItem("commentmain", JSON.stringify(commentArr));
   }
-  course_div_stocks.innerHTML = `
-                  <img class="bookmark-icon" id="bookmark-icon" onclick="toggle(${
-                    course_data[i]["id"]
-                  })" src=${course_data[i]["bookmark_img"]} alt="">
-                  <div>
-                                   <img class="course-img-stocks" src=${
-                                     course_data[i]["img"]
-                                   } alt="">
-                               </div>
-                               <a class="card-a" href="details.html?name=${
-                                 course_data[i]["title"]
-                               }">
-                                   <h3 class="course-title">${
-                                     course_data[i]["title"]
-                                   }</h3>
-                                   <div class="course-details">
-                                       <p class="course-timing">${
-                                         "<b>Dur :</b>" +
-                                         course_data[i]["timing"]
-                                       }</p>
-                                       <p class="course-ln">&emsp;${
-                                         "<b>Lang : </b>" +
-                                         course_data[i]["language"]
-                                       }</p>
-                                       <p class="course-enrolled">&emsp;${
-                                         "<b>Enr :</b>" +
-                                         course_data[i]["enrolled"] +
-                                         " " +
-                                         "Enrolled"
-                                       }</p>
-                                   </div>
-                                   <div class="flexcost">
-                                    <h3 class="course-cost">${
-                                      cost + " " + course_data[i]["cost"]
-                                    }</h3>
-                                <strike class="course-oldcost" >${
-                                  "₹" + " " + course_data[i]["old_cost"]
-                                }</strike>
-                                <p class="course-discount" >${
-                                  Math.floor(discount) + " " + "% discount"
-                                }</p>
-                                </div>
-                               </div> `;
-  console.log(course_div_stocks);
-
-  document
-    .querySelector(".scroll-courses-div-stocks")
-    .append(course_div_stocks);
+  location.reload();
 }
 
-//bookmark icon toggle button function
+// not to get value replace in the comment main array in the local storage
+if (localStorage.getItem("commentmain") !== null) {
+  commentArr = JSON.parse(window.localStorage.getItem("commentmain"));
+}
+console.log(commentArr);
 
-function toggle(id) {
-  let get_click_obj;
+let commentNewarr = [];
+let commentnewob = {};
+
+// to show the appropriate fundraise card comment
+// loop to assignit to a new object then push it into the new array and store it in a localstorage
+for (let i = 0; i < commentArr.length; i++) {
+  if (getIDdetails == commentArr[i]["fundraiseId"]) {
+    commentNewarr.push(commentArr[i]);
+  }
+}
+console.log(commentNewarr);
+window.localStorage.setItem("comment", JSON.stringify(commentNewarr));
+
+let comment = JSON.parse(window.localStorage.getItem("comment"));
+console.log(comment);
+
+// to read the comment by the donater
+
+for (let i = 0; i < comment.length; i++) {
+  console.log(comment[i]["user_ID"]);
+  console.log(user_id["userid"]);
+  if (comment[i]["user_ID"] == user_id["userid"]) {
+    let commentMsg = document.createElement("div");
+    commentMsg.setAttribute("class", "chat-message");
+
+    let userImg;
+    userImg = document.createElement("img");
+    userImg.setAttribute("src", comment[i]["userpic"]);
+    userImg.setAttribute("alt", "profile");
+    userImg.setAttribute("width", "32");
+    userImg.setAttribute("height", "32");
+    userImg.setAttribute("style", "border-radius: 50%");
+    commentMsg.append(userImg);
+
+    let msgcontent;
+    msgcontent = document.createElement("div");
+    msgcontent.setAttribute("class", "chat-message-content");
+    commentMsg.append(msgcontent);
+
+    let spanTime;
+    spanTime = document.createElement("div");
+    spanTime.setAttribute("class", "chat-time");
+    spanTime.innerText = comment[i]["time"];
+    msgcontent.append(spanTime);
+
+    let heading_user;
+    heading_user = document.createElement("h5");
+    heading_user.innerText = comment[i]["userName"];
+    msgcontent.append(heading_user);
+
+    let msg;
+    msg = document.createElement("p");
+    msg.setAttribute("style", "color: black");
+    msg.innerText = comment[i]["msg"];
+    msg.setAttribute("id", "msg_content");
+    msgcontent.append(msg);
+
+    let editDeldiv;
+    editDeldiv = document.createElement("div");
+    editDeldiv.setAttribute("class", "editdeldiv");
+    commentMsg.append(editDeldiv);
+
+    let edit;
+    edit = document.createElement("img");
+    edit.setAttribute("src", "../../Assets/images/draw.png");
+    edit.setAttribute("width", "20");
+    edit.setAttribute("height", "20");
+    edit.setAttribute("onclick", "edit(this.id)");
+    edit.setAttribute("id", comment[i]["commentId"]);
+    edit.setAttribute("class", "edit");
+    edit.setAttribute("style", "cursor:pointer");
+    editDeldiv.append(edit);
+
+    let del;
+    del = document.createElement("img");
+    del.setAttribute("src", "../../Assets/images/delete.png");
+    del.setAttribute("width", "20");
+    del.setAttribute("height", "20");
+    del.setAttribute("id", comment[i]["commentId"]);
+    del.setAttribute("onclick", "del(this.id)");
+    del.setAttribute("style", "cursor:pointer");
+    editDeldiv.append(del);
+
+    let hr;
+    hr = document.createElement("hr");
+
+    document.querySelector(".chat-history").append(commentMsg, hr);
+  } else {
+    let commentMsg = document.createElement("div");
+    commentMsg.setAttribute("class", "chat-message");
+    let userImg;
+    userImg = document.createElement("img");
+    userImg.setAttribute("src", comment[i]["userpic"]);
+    userImg.setAttribute("alt", "profile");
+    userImg.setAttribute("width", "32");
+    userImg.setAttribute("height", "32");
+    userImg.setAttribute("style", "border-radius: 50%");
+    commentMsg.append(userImg);
+    let msgcontent;
+    msgcontent = document.createElement("div");
+    msgcontent.setAttribute("class", "chat-message-content");
+    commentMsg.append(msgcontent);
+    let spanTime;
+    spanTime = document.createElement("div");
+    spanTime.setAttribute("class", "chat-time");
+    spanTime.innerText = comment[i]["time"];
+    msgcontent.append(spanTime);
+    let heading_user;
+    heading_user = document.createElement("h5");
+    heading_user.innerText = comment[i]["userName"];
+    msgcontent.append(heading_user);
+    let msg;
+    msg = document.createElement("p");
+    msg.setAttribute("style", "color: black");
+    msg.innerText = comment[i]["msg"];
+    msgcontent.append(msg);
+    let hr;
+    hr = document.createElement("hr");
+    document.querySelector(".chat-history").append(commentMsg, hr);
+  }
+}
+
+// function edit() {
+//   document.getElementById("editfieldset").classList.toggle("active");
+// }
+
+// to edit the comment
+function edit(id) {
   console.log(id);
+  window.localStorage.setItem("commentId", JSON.stringify(id));
+  document.getElementById("editfieldset").classList.toggle("active");
+}
 
-  //find clicked course
-  get_click_obj = course_data.find((e) => e.id === id);
-  console.log(get_click_obj);
+let form;
+form = document.createElement("form");
 
-  //adding key bookmark = true
-  get_click_obj.bookmark = true;
+let form2;
+form2 = document.createElement("form");
+form.prepend(form2);
 
-  //create object to assign
-  let bookmark_obj = {
-    bookmark_img: "../assets/images/icons8-add-bookmark-50_bookmarked.png",
+let fieldset2;
+fieldset2 = document.createElement("fieldset");
+fieldset2.setAttribute("id", "editfieldset");
+form2.append(fieldset2);
+
+let editinput;
+editinput = document.createElement("input");
+editinput.setAttribute("type", "text");
+editinput.setAttribute("autocomplete", "off");
+editinput.setAttribute("id", "editinput");
+editinput.setAttribute("placeholder", "edit Your message");
+fieldset2.append(editinput);
+
+let sendIcon2;
+sendIcon2 = document.createElement("img");
+sendIcon2.setAttribute("src", "../../Assets/images/send-message.png");
+sendIcon2.setAttribute("alt", "send");
+sendIcon2.setAttribute("class", "send");
+sendIcon2.setAttribute("onclick", "sendedit()");
+sendIcon2.setAttribute("width", "20");
+sendIcon2.setAttribute("height", "20");
+sendIcon2.setAttribute("style", "padding-top: 5px");
+fieldset2.append(sendIcon2);
+
+let fieldset;
+fieldset = document.createElement("fieldset");
+form.append(fieldset);
+
+let inputMsg;
+inputMsg = document.createElement("input");
+inputMsg.setAttribute("type", "text");
+inputMsg.setAttribute("autocomplete", "off");
+inputMsg.setAttribute("id", "msg");
+inputMsg.setAttribute("placeholder", "Send your message");
+fieldset.append(inputMsg);
+
+let sendIcon;
+sendIcon = document.createElement("img");
+sendIcon.setAttribute("src", "../../Assets/images/send-message.png");
+sendIcon.setAttribute("alt", "send");
+sendIcon.setAttribute("class", "send");
+sendIcon.setAttribute("onclick", "send()");
+sendIcon.setAttribute("width", "20");
+sendIcon.setAttribute("height", "20");
+sendIcon.setAttribute("style", "padding-top: 5px");
+fieldset.append(sendIcon);
+
+document.querySelector(".chat-history").append(form);
+
+let userCommentId = JSON.parse(window.localStorage.getItem("commentId"));
+console.log(userCommentId);
+let userallComments = JSON.parse(window.localStorage.getItem("commentmain"));
+
+let Com = userallComments.find(function (comment) {
+  let Comment = comment["commentId"];
+
+  if (userCommentId == Comment) {
+    return true;
+  }
+});
+console.log(Com);
+
+function sendedit() {
+  let msg = document.getElementById("editinput").value;
+
+  let neweditObj = {
+    msg,
   };
+  console.log(neweditObj);
+  let merge;
+  merge = Object.assign(Com, neweditObj);
+  let usereditmsg;
+  usereditmsg = userallComments.indexOf(Com);
+  userallComments[usereditmsg] = merge;
+  window.localStorage.setItem("commentmain", JSON.stringify(userallComments));
+  location.reload();
+}
 
-  //assign this source with previous source
-  let assignObj = Object.assign(get_click_obj, bookmark_obj);
-  console.log(assignObj);
+// to delete the comment
+function del(commentid) {
+  let commentINdex = userallComments.indexOf(Com);
 
-  //find clicked object index
-  let index = course_data.indexOf(get_click_obj);
-  console.log(index);
+  let alertComfirm = confirm("Are you sure you want to delete this comment");
 
-  //assign new value to old one
-  course_data[index] = assignObj;
-
-  //set new value to localstorage
-  localStorage.setItem("course_data", JSON.stringify(course_data));
-
-  console.log(get_click_obj);
-
-  if (localStorage.getItem("bookmarks") != null) {
-    //arrary already exist
-    get_click_obj_arr = JSON.parse(localStorage.getItem("bookmarks"));
-
-    //bookmark local parse array
-    const bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
-    console.log(bookmarks);
-
-    //check for duplicates
-    const already_exist_obj = bookmarks.find((e) => e.id === get_click_obj.id);
-    console.log(already_exist_obj);
-
-    if (already_exist_obj == undefined) {
-      get_click_obj.bookmark = true;
-      get_click_obj_arr.push(get_click_obj);
-      localStorage.setItem("bookmarks", JSON.stringify(get_click_obj_arr));
-      alert("course Added to Bookmarks");
-      window.location.href = "learn.html";
-    } else {
-      alert("course Already Bookmarked");
-      window.location.href = "learn.html";
-    }
+  if (alertComfirm != true) {
+    return;
   } else {
-    //create bookmark array
-    let get_click_obj_arr = [];
-
-    get_click_obj.bookmark = true;
-    get_click_obj_arr.push(get_click_obj);
-
-    localStorage.setItem("bookmarks", JSON.stringify(get_click_obj_arr));
-    alert("course Added to Bookmarks...");
+    window.localStorage.setItem("commentId", JSON.stringify(commentid));
+    userallComments.splice(commentINdex, 1);
+    window.localStorage.setItem("commentmain", JSON.stringify(userallComments));
+    location.reload();
   }
 }
