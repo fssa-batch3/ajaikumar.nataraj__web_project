747c4ae3bf00943b723c05aef30852e985790a96
Ajaikumar-N <ajaikumar.nataraj@fssa.freshworks.com>
2023-05-02 15:17:27 +0530

    chore: seller's js file cretae complete

---
+++ assets/JavaScript/seller_upload.js
@@ -0,0 +1,69 @@
+function submit() {
+  window.location.href = "/pages/11.place-products.html";
+}
+function history() {
+  window.location.href = "/pages/10.a.history.html";
+}
+
+// compare the user's detail from the login email to get details of uploader
+uploadForm = document.getElementById("form");
+let id = JSON.parse(localStorage.getItem("user_info"));
+console.log(id);
+
+let login_id = JSON.parse(localStorage.getItem("logIn"));
+console.log(login_id);
+
+const get_obj = id.find((e) => e.Email === login_id);
+console.log(get_obj);
+
+document.createElement("newly_added");
+
+// to get details of the products from the form by id and store in the new array
+uploadForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let array = [];
+  if (localStorage.getItem("newly_added") != null) {
+    array = JSON.parse(localStorage.getItem("newly_added"));
+  }
+
+  // let match = false;
+
+  // to set the details by object in an array
+  const Url = document.getElementById("url").value;
+  const Name = document.getElementById("product").value;
+  const Type = document.getElementById("type").value;
+  const Price = document.getElementById("price").value;
+  const Qty = document.getElementById("qty").value;
+  const bQty = document.getElementById("qty").value;
+  // const Place = document.getElementById("place").value;
+  const Description = document.getElementById("description").value;
+  let ProductId = Date.now();
+  let userId = get_obj["id"];
+  // let date = get_obj["transport_DOB"];
+
+  let inputDetails = {
+    Url,
+    Name,
+    Type,
+    Price,
+    Qty,
+    bQty,
+    // Place,
+    Description,
+    ProductId,
+    userId,
+    // date,
+  };
+  // console.log(Product_id);
+
+  array.push(inputDetails);
+  const str = JSON.stringify(array);
+  localStorage.setItem("newly_added", str);
+  console.log(str);
+  document.getElementById("form").reset();
+});
+
+function profile() {
+  window.location.href = "/pages/profile.html";
+}
+// const start = Date.now();
