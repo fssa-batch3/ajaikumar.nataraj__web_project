689954f32fe28c07e611e26d467b9fa87b258852
Ajaikumar-N <ajaikumar.nataraj@fssa.freshworks.com>
2023-05-02 17:57:01 +0530

    chore: worker js file create completed

---
+++ assets/JavaScript/worker_home.js
@@ -0,0 +1,34 @@
+const oneUser = JSON.parse(localStorage.getItem("worker_login"));
+
+const user_info = JSON.parse(localStorage.getItem("worker_info"));
+let select_user = user_info.find(function (event) {
+  let customerEmail = event["Email"];
+  if (user_info == customerEmail) {
+    return true;
+  } else if (oneUser == customerEmail) {
+    return true;
+  }
+});
+
+const name = document.getElementById("profile");
+
+name.value = select_user["FullName"];
+
+function new_order() {
+  window.location.href = "/pages/worker_new_order.html";
+}
+function new_upload() {
+  window.location.href = "/pages/worker_new_upload.html";
+}
+function deliver_order_inprogress() {
+  window.location.href = "/pages/worker_order_inprogress.html";
+}
+function pickup_order_inprogress() {
+  window.location.href = "/pages/worker_pickup_inprogress.html";
+}
+function customer() {
+  window.location.href = "/pages/buyer.html";
+}
+function seller() {
+  window.location.href = "/pages/seller.html";
+}
