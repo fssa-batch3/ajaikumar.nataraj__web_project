9d2781995e504b0585e892889bd821c3e30cd65e
Ajaikumar-N <ajaikumar.nataraj@fssa.freshworks.com>
2023-05-03 18:33:53 +0530

    chore: js page create completed

---
+++ assets/JavaScript/buyer_total.js
@@ -0,0 +1,64 @@
+// set place for show total amount
+let total_value = document.getElementById("operoation");
+total_value.append("₹" + total);
+
+// to get details of the users for delivery ..
+const oneUser = JSON.parse(localStorage.getItem("buyer_logIn"));
+
+const user_info = JSON.parse(localStorage.getItem("buyer_info"));
+let select_user = user_info.find(function (event) {
+  let customerEmail = event["Email"];
+  if (user_info == customerEmail) {
+    return true;
+  } else if (oneUser == customerEmail) {
+    return true;
+  }
+});
+
+console.log(select_user);
+// to get value from delivery form
+const phNo = document.getElementById("Phone_Number");
+const userName = document.getElementById("First_name");
+const Land_Address = document.getElementById("Land_Address");
+
+// Compare the values
+
+phNo.value = select_user["Phone_number"];
+userName.value = select_user["FullName"];
+Land_Address.value = select_user["Land_Address"] || " ";
+
+// to replace the values
+const form = document.getElementById("form");
+form.addEventListener("change", function (event) {
+  event.preventDefault();
+
+  let FullName = document.getElementById("First_name").value;
+  let Phone_number = document.getElementById("Phone_Number").value;
+  let Land_Address = document.getElementById("Land_Address").value;
+
+  let newData = {
+    FullName,
+    Phone_number,
+    Land_Address,
+  };
+  console.log(newData);
+
+  // code for change the delivery address
+  const combineData = Object.assign(select_user, newData);
+  console.log(combineData);
+
+  let findIndex = user_info.indexOf(select_user);
+  user_info[findIndex] = combineData;
+  localStorage.setItem("buyer_info", JSON.stringify(user_info));
+});
+
+function order() {
+  window.location.href = "/pages/8.payment-page.html";
+}
+
+function back() {
+  window.location.href = "/pages/6.buy-2.html";
+}
+function profile() {
+  window.location.href = "/pages/profile_buy.html";
+}
---
+++ assets/JavaScript/seller_self_bill.js
@@ -0,0 +1,63 @@
+// to set the total amount
+let total_value = document.getElementById("operoation");
+total_value.append("₹" + total);
+
+// to get the user's detail by comparing login email
+const oneUser = JSON.parse(localStorage.getItem("logIn"));
+const user_info = JSON.parse(localStorage.getItem("user_info"));
+let select_user = user_info.find(function (event) {
+  let customerEmail = event["Email"];
+  if (user_info == customerEmail) {
+    return true;
+  } else if (oneUser == customerEmail) {
+    return true;
+  }
+});
+
+console.log(select_user);
+// to get value from registration form
+const phNo = document.getElementById("Phone_Number");
+const userName = document.getElementById("First_name");
+// const transport_DOB = document.getElementById("Pickup_date");
+const Land_Address = document.getElementById("Land_Address");
+
+// Compare the values
+phNo.value = select_user["Phone_number"];
+userName.value = select_user["FullName"];
+// transport_DOB.value = select_user["transport_DOB"];
+Land_Address.value = select_user["Land_Address"] || "";
+
+// to replace the values
+const form = document.getElementById("form");
+form.addEventListener("change", function (event) {
+  event.preventDefault();
+
+  let FullName = document.getElementById("First_name").value;
+  let Phone_number = document.getElementById("Phone_Number").value;
+  // let transport_DOB = document.getElementById("Pickup_date").value;
+  let Land_Address = document.getElementById("Land_Address").value;
+
+  let newData = {
+    FullName,
+    Phone_number,
+    // transport_DOB,
+    Land_Address,
+  };
+  console.log(newData);
+
+  // assign the changed value
+  const combineData = Object.assign(select_user, newData);
+  console.log(combineData);
+  // alert("successfully changed");
+
+  let findIndex = user_info.indexOf(select_user);
+  user_info[findIndex] = combineData;
+  localStorage.setItem("user_info", JSON.stringify(user_info));
+});
+
+function account() {
+  window.location.href = "/pages/13.Acct.detail.html";
+}
+function profile() {
+  window.location.href = "/pages/profile.html";
+}
